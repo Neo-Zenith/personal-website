40e36b305e9edb3f10c581181ca76c1138785537
Ubuntu <neozenith@neozenith.wlqg4eh15veenpfmgefhhdaqea.ix.internal.cloudapp.net>
2024-07-19 08:42:27 +0000

    Added host option for external viewing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    host: true, // This will expose the server to the local network
+    port: 3000 // You can specify any port you prefer
+  }
 });
